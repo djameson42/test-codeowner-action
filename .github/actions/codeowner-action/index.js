6a6a962898ce9d56470304e2e955c3f4e2eddb25
David Jameson <david.mck.jameson@gmail.com>
2022-09-20 07:35:59 +1200

    first pass at auth

--- .github/actions/codeowner-action/index.js
+++ .github/actions/codeowner-action/index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const token = `token ${core.getInput("access-token")}`;
+
+const JSON_ACCEPT_HEADER = {
+  "Accept": "application/vnd.github.v3+json",
+  "Authorization": token
+};
+
+const getPullRequestFiles = (url) => {
+  console.log("Getting new schema files");
+  return axios.get(url + "/files?per_page=50", {
+    headers: JSON_ACCEPT_HEADER
+  });
+};
+
+const payload = github.context.payload;
+console.log(`Event payload: ${JSON.stringify(payload, undefined, 2)}`);
+
+console.log(getPullRequestFiles(payload.pull_request.url));
+
+
+
+
